1bb12ee218dbc7ca0c9a437a808a3b9d6fe8900c
RomanK <namor925@gmail.com>
2020-04-21 18:50:21 +0700

    before add user model

---
+++ src/main/java/web/controller/listAllUser.java
@@ -0,0 +1,21 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class listAllUser {
+
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public String returnListUsers(ModelMap model) {
+        List<User> list = new LinkedList<>();
+
+        return "listAllUsers";
+    }
+}
